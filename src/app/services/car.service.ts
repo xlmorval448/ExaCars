973d047fe638089db0c31746075e56a9ec49c41a
xlmorval448 <xlmorval448@ieshnosmachado.org>
2026-01-28 10:55:03 +0100

    API, Navbar y Home.

---
+++ src/app/services/car.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+export interface Car {
+  id: number;
+  brand: string;
+  model: string;
+  price: number;
+  hp: number;
+  year: number;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+  private apiUrl = 'https://api.npoint.io/3fa9910245a279be5709'; 
+
+  constructor(private http: HttpClient) { }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => response.cars)
+    );
+  }
+}
